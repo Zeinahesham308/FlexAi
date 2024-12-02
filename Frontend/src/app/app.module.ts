8b0b1c6d0be335bad02b4018ca97308ef87c83c1
SalmaWaleeddd <salmawaleed2030@outlook.com>
2024-12-02 12:26:20 +0200

    added signUp component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    LoginComponent
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Frontend/src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { QuestionsService } from './services/questions.service';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrl: './sign-up.component.scss'
+})
+export class SignUpComponent implements OnInit {
+
+  genderChosen:boolean = false;
+  questionsList:any[]=[];
+
+  constructor(private questionsService:QuestionsService){}
+
+  setGender(){
+    this.genderChosen = true;
+  }
+
+  ngOnInit(): void {
+    this.questionsService.getQuestions().subscribe((data)=>{
+      this.questionsList=data;
+    });
+  }
+}
