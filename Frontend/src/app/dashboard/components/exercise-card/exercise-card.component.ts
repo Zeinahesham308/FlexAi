d15697260c3cd240ab67e2afd9c88693ac4efeb8
SalmaWaleeddd <salmawaleed2030@outlook.com>
2025-06-17 18:03:48 +0300

    Added excersie card component

--- Frontend/src/app/dashboard/components/exercise-card/exercise-card.component.ts
+++ Frontend/src/app/dashboard/components/exercise-card/exercise-card.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+interface Exercise {
+  id: string;
+  name: string;
+  muscleGroup: string;
+  sets?: number;
+  reps?: number;
+  link?: string;
+}
 @Component({
   selector: 'app-exercise-card',
   standalone: false,
-  
+
   templateUrl: './exercise-card.component.html',
   styleUrl: './exercise-card.component.scss'
 })
 export class ExerciseCardComponent {
+  @Input() exercise: Exercise = {
+    id: '1',
+    name: 'Default Exercise',
+    muscleGroup: 'General',
+    sets: 3,
+    reps: 10,
+    link: " "
+  };
+
+  @Input() showActions: boolean = true;
+  @Output() exerciseDeleted = new EventEmitter<string>();
+
+
+  deleteExercise() {
+    if (confirm('Are you sure you want to delete this exercise?')) {
+      this.exerciseDeleted.emit(this.exercise.id);
+    }
+  }
+
+
 
 }
